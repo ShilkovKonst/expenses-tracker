5fc7146bd27d08f22d434ef6d6d8214cee594208
Konstantin Shilkov <konst.shilkov@gmail.com>
2026-03-01 17:28:59 +0100

    refactor: streamline ChartsBlock component by removing commented code and enhancing tag selection logic

--- src/components/chartsBlock/ChartsBlock.tsx
+++ src/components/chartsBlock/ChartsBlock.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ChartsHeader from "./ChartsHeader";
 import OverviewBlock from "./OverviewBlock";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import RecordsCharts from "./RecordsCharts";
 import { useTracker } from "@/context/TrackerContext";
 import { MonthRecord, TrackerYears } from "@/lib/types/dataTypes";
@@ -20,28 +20,26 @@ const ChartsBlock = () => {
     [trackerYears],
   );
 
+  useEffect(() => {
+    setSelectedTag(createTagId(-1));
+  }, [trackerId]);
+
   return (
     <section className="w-full lg:w-1/2 2xl:w-2/3 lg:pt-1 pb-3 lg:pb-7 lg:pr-6 lg:pl-4 lg:border-l-3 border-blue-400">
       {isLoading ? (
         <LoadingSkeleton rowNo={1} />
       ) : !trackerId ? (
-        <>
-          {/* <NoDataPlaceholder
-            text={t(locale, "body.form.tracker.idsTitleEmptyCharts")}
-          /> */}
-          <Image
-            className="opacity-1 m-auto hidden lg:block"
-            width={680}
-            height={665}
-            src={"/tracker-logo.svg"}
-            alt="tracker-logo"
-          />
-        </>
+        <Image
+          className="opacity-1 m-auto hidden lg:block"
+          width={680}
+          height={665}
+          src={"/tracker-logo.svg"}
+          alt="tracker-logo"
+        />
       ) : (
         <>
           <ChartsHeader setSelectedTag={setSelectedTag} />
           <OverviewBlock selectedTag={selectedTag} rawRecords={rawRecords} />
-          {/* <div className="pt-4 border-b-6 border-blue-400"></div> */}
           {rawRecords.length > 0 && <RecordsCharts selectedTag={selectedTag} />}
         </>
       )}
